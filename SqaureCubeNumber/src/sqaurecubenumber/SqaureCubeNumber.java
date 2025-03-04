2821297335a07aaa768e6514432eb97dc8220c46
LeahBasson <leahbasson54@gmail.com>
2025-03-04 19:43:53 +0200

    :rocket:

---
+++ SqaureCubeNumber/src/sqaurecubenumber/SqaureCubeNumber.java
@@ -0,0 +1,16 @@
+
+package sqaurecubenumber;
+
+
+public class SqaureCubeNumber {
+
+    public static void main(String[] args) {
+        double number = 2;
+        double squareOfNumber = Math.pow(number, number);
+        double cubeOfNumber = Math.pow(number, 4);
+        
+        System.out.println("Square of number: " + squareOfNumber);
+        System.out.println("Cube of number: " + cubeOfNumber);
+    }
+    
+}
